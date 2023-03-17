12e100307bacaeecac302fa2b633b65773310e70
Subhan Khan <subhank1997@gmail.com>
2023-03-17 00:32:36 -0400

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,11 @@
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
-import React, { useState, useEffect, useRef } from "react";
-import BIRDS from "vanta/dist/vanta.birds.min";
-import * as THREE from "three";
+import React from "react";
 
 const Home = () => {
-    const [vantaEffect, setVantaEffect] = useState(0);
-    const vantaRef = useRef(null);
-
-    useEffect(() => {
-        if (!vantaEffect) {
-            setVantaEffect(
-                BIRDS({
-                    el: vantaRef.current,
-                    THREE: THREE,
-                    mouseControls: true,
-                    touchControls: true,
-                    gyroControls: false,
-                    minHeight: 200.00,
-                    minWidth: 200.00,
-                    scale: 1.00,
-                    scaleMobile: 1.00
-                })
-            );
-        }
-        return () => {
-            if (vantaEffect) vantaEffect.destroy();
-        };
-    }, [vantaEffect]);
 
     return (
-        <div ref={vantaRef} name='home' className='w-full h-screen '>
+        <div name='home' className='w-full h-screen '>
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600 font-bold'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Subhan Khan</h1>
@@ -41,18 +16,18 @@ const Home = () => {
                     to tech with a strong interest in full-stack development and working in cross-functional
                     teams.
                 </p>
-                <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                        View Work
-                        <Link to="projects" smooth={true} offset={50} duration={500}>
-                        <span className='group-hover:rotate-90 duration-300'>
-                            <HiArrowNarrowRight className='ml-2' />
-                        </span>
+                <div className='w-fit'>
+                    <Link to="projects" smooth={true} offset={50} duration={500}>
+                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                            View Work
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <HiArrowNarrowRight className='ml-2' />
+                            </span>
+                        </button>
                     </Link>
-                </button>
+                </div>
             </div>
         </div>
-        </div >
     )
 }
 
