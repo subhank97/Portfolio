c8225bfe7840582a2b238995cfaae88b4d78b6b9
Subhan Khan <subhank1997@gmail.com>
2023-03-16 23:35:42 -0400

    vanta config

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
+import React, { useState, useEffect, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.birds.min";
+import * as THREE from "three";
 
 const Home = () => {
+    const [vantaEffect, setVantaEffect] = useState(0);
+    const vantaRef = useRef(null);
+  
+    useEffect(() => {
+      if (!vantaEffect) {
+        setVantaEffect(
+          BIRDS({
+            el: vantaRef.current,
+            THREE: THREE,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 1.00,
+            scaleMobile: 1.00
+          })
+        );
+      }
+      return () => {
+        if (vantaEffect) vantaEffect.destroy();
+      };
+    }, [vantaEffect]);
 
     return (
-        <div name='home' className='w-full h-screen '>
+        <div ref={vantaRef} name='home' className='w-full h-screen '>
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-pink-600'>Hi, my name is</p>
+                <p className='text-pink-600 font-bold'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Subhan Khan</h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
